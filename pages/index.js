691da36dc317879afcf0f5e369abf04b16ab9e59
Misael Gomes <102962890+misassauro@users.noreply.github.com>
2023-12-02 23:44:05 +0000

    modificando a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Ainda vou pensar numa frase bonitinha. Aguarde.</h1>
+    return <h1>Oi! Terminei de ler o primeiro volume de Sherlock Holmes há um tempinho. Se ainda tiver interesse em ler, dá uma risadinha. Se não der risadinha, prepare-se para ter o encanamento entupido com bananas.</h1>
 }
 
 export default Home;
