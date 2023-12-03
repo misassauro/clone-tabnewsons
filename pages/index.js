e9eaf351c95138243ea759993ede54e1e6f7ec87
Misael Gomes <102962890+misassauro@users.noreply.github.com>
2023-12-03 01:29:10 +0000

    risadinha confirmed?

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 function Home() {
     return <div>
-        <p>Um dia eu vou deixar esse treco "usável", daí você vai ser a primeira pessoa a digitar alguma coisa aqui embaixo de um modo que eu veja.</p><br></br>
-        <input type="text" placeholder="Bem aqui..."></input> <br></br>
-        <p>Até lá, paciência. Espero conseguir antes de você terminar de ler Sherlock KKKKKKKKK <s>leia devagar, por favor.</s></p>
+        <h2>Você deu uma risadinha?</h2>
     </div>
 }
 
