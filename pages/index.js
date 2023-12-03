880b271ee684ce5a7414e7d4e1d1d3ee1898f3ad
Misael Gomes <102962890+misassauro@users.noreply.github.com>
2023-12-03 01:09:48 +0000

    outro deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Peraí...</h1>
+    return <div>
+        <p>Um dia eu vou deixar esse treco "usável", daí você vai ser a primeira pessoa a digitar alguma coisa aqui embaixo de um modo que eu veja.</p><br></br>
+        <input type="text" placeholder="Bem aqui..."></input> <br></br>
+        <p>Até lá, paciência. Espero conseguir antes de você terminar de ler Sherlock KKKKKKKKK <s>leia devagar, por favor.</s></p>
+    </div>
 }
 
 export default Home;
