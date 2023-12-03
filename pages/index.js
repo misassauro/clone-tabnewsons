4bf37769ef490ac13ba82e177ef69b1b73f5c992
Misael Gomes <102962890+misassauro@users.noreply.github.com>
2023-12-03 00:25:50 +0000

    modificando a mensagem de novo :)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Oi! Terminei de ler o primeiro volume de Sherlock Holmes há um tempinho. Se ainda tiver interesse em ler, dá uma risadinha. Se não der risadinha, prepare-se para ter o encanamento entupido com bananas.</h1>
+    return  <em>Oi! ✨✨✨ Espero que esteja bem e que esse recado não te aborreça de alguma forma.<br></br><br></br> 
+            Só queria dizer que eu terminei de ler o primeiro volume de Sherlock Holmes há um tempinho. 
+            Continuo querendo muito te emprestar. <br></br> Se ainda tiver interesse em ler, dá uma risadinha.<br></br>
+            Saiba que vai estar tudo bem se você decidir ignorar essa mensagem; eu entendo perfeitamente. <s>Apenas se prepare para ter o encanamento entupido com 🍌bananas🍌, muitas bananas.</s><br></br><br></br> 
+            Um abraço humano e dois abraços felinos, já que eles são "uma cura pro coração".<br></br><br></br>
+            De seu irmão que te quer bem hoje e sempre, <br></br>Misa do El.</em>
 }
 
 export default Home;
